af49b7a3227b1b32b505a5d86ddf234077de1d09
Marc Lennox <marc.lennox@gmail.com>
2015-08-06 22:16:32 -0400

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 # Look for known command aliases
 case "$1" in
-  "etcd2env" ) shift; command="/usr/local/bin/etcd2env" "$@";;
-  *          ) command="/usr/local/bin/etcdctl" "$@" ;;
+  "etcd2env" ) shift; exec /usr/local/bin/etcd2env "$@";;
+  *          ) exec /usr/local/bin/etcdctl "$@" ;;
 esac
-
-exec ${command}
