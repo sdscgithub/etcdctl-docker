3f5b4834e53e30e8516a03949ce17af6b77e2d6c
Marc Lennox <marc.lennox@gmail.com>
2015-08-08 15:09:15 -0400

    Changed so that it won't steamroll existing values unless retrieving is successful

--- etcd2env.sh
+++ etcd2env.sh
@@ -5,14 +5,15 @@ set -e
 ETCD_ENVIRONMENT_VARIABLE_REGEX="^\s*ETCD2ENV_([^=]+)=(.+)\s*$"
 OUTPUT_PIPE="${1:-/dev/stdout}"
 
-# Make sure we blank out the pipe if it's a file
-printf '' > "${OUTPUT_PIPE}"
-
 # Iterate through each matching environment variable and convert it to the output
 while read -r env_name etcd_key ; do
   env_value=$(etcdctl get "${etcd_key}" || true)
   if [ -z "${env_value}" ]; then
     echo >&2 "Unable to get value for '${etcd_key}' from etcd."
+    exit 1
   fi
-  printf '%s=%s\n' "${env_name}" "${env_value}" >> "${OUTPUT_PIPE}"
+  environment=$(printf '%s%s=%s\n' "${environment}" "${env_name}" "${env_value}")
 done < <(env | grep -E "${ETCD_ENVIRONMENT_VARIABLE_REGEX}"  | sed -E "s/${ETCD_ENVIRONMENT_VARIABLE_REGEX}/\1\t\2/")
+
+# Send the results to the output pipe
+echo -e "${environment}" > "${OUTPUT_PIPE}"
