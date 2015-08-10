bf8debefc1d64938c18e0a5ef8f3a064ed6621ab
Marc Lennox <marc.lennox@gmail.com>
2015-08-10 11:56:01 -0400

    Add the ability to change output format and environment prefix for etcd2env

--- etcd2env.sh
+++ etcd2env.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Set environment variables
-ETCD_ENVIRONMENT_VARIABLE_REGEX="^\s*ETCD2ENV_([^=]+)=(.+)\s*$"
+prefix="${ETCD2ENV_PREFIX:-ETCD2ENV_}"
+format="${ETCD2ENV_FORMAT:-DOTENV}"
+unset ETCD2ENV_PREFIX
+unset ETCD2ENV_FORMAT
+ETCD_ENVIRONMENT_VARIABLE_REGEX="^\s*${prefix}([^=]+)=(.+)\s*$"
 OUTPUT_PIPE="${1:-/dev/stdout}"
 
 # Iterate through each matching environment variable and convert it to the output
@@ -12,7 +16,8 @@ while read -r env_name etcd_key ; do
     echo >&2 "Unable to get value for '${etcd_key}' from etcd."
     exit 1
   fi
-  environment="${environment}${env_name}=${env_value}\n"
+  [ "${format}" = "DOTENV" ] && environment="${environment}${env_name}=${env_value}\n"
+  [ "${format}" = "BASH" ] && environment="${environment}export ${env_name}=\"${env_value}\"\n"
 done < <(env | grep -E "${ETCD_ENVIRONMENT_VARIABLE_REGEX}"  | sed -E "s/${ETCD_ENVIRONMENT_VARIABLE_REGEX}/\1\t\2/")
 
 # Send the results to the output pipe
